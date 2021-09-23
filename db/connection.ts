5041bf8f2eef306556213a680801d404b40c6378
Fernando Monroy <pulmundo@gmail.com>
2021-09-22 20:17:55 -0600

    Add credentials heroku

--- db/connection.ts
+++ db/connection.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('node', 'root', '', {
-    host: 'localhost',
+const db = new Sequelize('heroku_598927b1f7bb515', 'b00ea2cdcc89b9', '2e4734e9', {
+    host: 'us-cdbr-east-04.cleardb.com',
     dialect: 'mariadb',
     //logging: false
 });
